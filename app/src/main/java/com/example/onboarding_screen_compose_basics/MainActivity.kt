10c6d4f3f6755423ea561ae3d3c0cae761d17159
abdulhakeemsonos <abdulhakeem.adefioye@sonos.com>
2024-07-23 22:03:30 -0500

    Added primary background to greeting text and added padding around text

---
+++ app/src/main/java/com/example/onboarding_screen_compose_basics/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.onboarding_screen_compose_basics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.onboarding_screen_compose_basics.ui.theme.OnboardingscreencomposebasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OnboardingscreencomposebasicsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    Greeting("Adeshola")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OnboardingscreencomposebasicsTheme {
+        Greeting("Adeshola")
+    }
+}
